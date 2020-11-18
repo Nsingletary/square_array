3aa79cdd7f5e39db59545896a129576e8e3d7763
Nsingletary <singletarybush@gmail.com>
2020-11-18 13:31:37 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(array)
   squared_array = []
   array.each do |number|
-    squared_array.push(number**2)
-  end
-  return squared array
-end 
+    squared_array.push (number**2)
+end
